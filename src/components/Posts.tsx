ecb00f10a41c90a1ed5e3dfeb92e226ec8d530b0
Adrián <ardillan@me.com>
2023-11-01 23:28:54 +0100

    fix: remove unnecesary code

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -4,26 +4,23 @@ import Link from "next/link";
 
 import styles from "./Posts.module.css";
 
-const Posts = ({ allPostsData }): JSX.Element => {
-  console.log(allPostsData.slice(0, 5));
-  return (
-    <ul className={styles.posts}>
-      {allPostsData
-        .slice(0, 5)
-        .map((post: { id: number; date: string; title: string }) => {
-          return (
-            <li key={post.id}>
-              <Link href={`/blog/${post.id}`}>
-                {post.title}
-                <small>
-                  <Date dateString={post.date} />
-                </small>
-              </Link>
-            </li>
-          );
-        })}
-    </ul>
-  );
-};
+const Posts = ({ allPostsData }): JSX.Element => (
+  <ul className={styles.posts}>
+    {allPostsData
+      .slice(0, 5)
+      .map((post: { id: number; date: string; title: string }) => {
+        return (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>
+              {post.title}
+              <small>
+                <Date dateString={post.date} />
+              </small>
+            </Link>
+          </li>
+        );
+      })}
+  </ul>
+);
 
 export default Posts;
