4d5ac7706a342ca2403875af6b5fdd14d13c2bdd
Adrián <ardillan@me.com>
2024-01-09 22:21:38 +0100

    fix: avoid caching data

--- src/lib/getGames.tsx
+++ src/lib/getGames.tsx
@@ -1,7 +1,10 @@
 const getNotionGames = async () => {
   const baseURL =
     "https://ardi-land-next.vercel.app" || "http://localhost:3000";
-  const notionGamesResponse = await fetch(`${baseURL}/api/notion`);
+  const notionGamesResponse = await fetch(`${baseURL}/api/notion`, {
+    next: { revalidate: 3600 },
+  });
+
   const notionGames = await notionGamesResponse.json();
 
   return notionGames;
