4de3321eddc60ec41dff1f6be0d9d17e465e7d2f
Adrián <ardillan@me.com>
2024-01-09 20:18:25 +0100

    fix: add SSL protocol

--- src/lib/getGames.tsx
+++ src/lib/getGames.tsx
@@ -1,5 +1,6 @@
 const getNotionGames = async () => {
-  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
+  const baseURL =
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || "http://localhost:3000";
   const notionGamesResponse = await fetch(`${baseURL}/api/notion`);
   const notionGames = await notionGamesResponse.json();
 
